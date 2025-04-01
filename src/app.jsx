a32b6ca2351032d78df9bf8cf372b0dbcd1a3db7
asean-onpu <asean.onpu.ex@gmail.com>
2025-04-02 06:30:21 +0900

    再レンダリング

--- src/app.jsx
+++ src/app.jsx
@@ -1,12 +1,29 @@
-import { useState} from "react";
+import { useEffect, useState} from "react";
 import { ColorfulMessage } from "./components/ColorfulMessage";
 
 export const App = () => {
+  console.log("--App--")
   const [num, setNum ] = useState(0);
+  const [isShowFace, setIsShowFace] = useState(false);
+
   const oncClickCountuUp = () => {
     setNum((prev) => prev + 1);
-    setNum((prev) => prev + 1);
-  }
+  };
+
+  const onClickTpggle = () => {
+    setIsShowFace(!isShowFace);
+  };
+
+  useEffect(() => {
+     console.log("--useEffect--")
+     if (num > 0){
+      if(num % 3 == 0) {
+        isShowFace || setIsShowFace(true);
+      } else {
+        isShowFace && setIsShowFace(false);
+      }
+    }
+  }, [num]);
 
   return (
     <>
@@ -15,6 +32,8 @@ export const App = () => {
       <ColorfulMessage color="green" >元気です！</ColorfulMessage>
       <button onClick={oncClickCountuUp}>カウントアップ</button>
       <p>{num}</p>
+      <button onClick={onClickTpggle}>on/off</button>
+      {isShowFace && <p>(*´ω｀*)</p>}      
     </>
   );
 };
